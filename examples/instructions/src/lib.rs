c848f6143d5f19f3663676ea6be47d7bfd87dab0
Blaine Bublitz <blaine@arcjet.com>
2025-09-23 13:35:01 -0700

    s16 and u16 inst and examples

--- examples/instructions/src/lib.rs
+++ examples/instructions/src/lib.rs
@@ -19,4 +19,16 @@ impl Guest for InstructionsWorld {
     fn u8_from_i32() -> u8 {
         Default::default()
     }
+    fn i32_from_s16(val: i16) {
+        assert!((i16::MIN..=i16::MAX).contains(&val));
+    }
+    fn s16_from_i32() -> i16 {
+        Default::default()
+    }
+    fn i32_from_u16(val: u16) {
+        assert!((u16::MIN..=u16::MAX).contains(&val));
+    }
+    fn u16_from_i32() -> u16 {
+        Default::default()
+    }
 }
