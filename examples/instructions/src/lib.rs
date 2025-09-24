47d93e05cf9a885ba32908fe142841e192df28a5
Blaine Bublitz <blaine@arcjet.com>
2025-09-24 08:14:53 -0700

    make them roundtrips to test casting

--- examples/instructions/src/lib.rs
+++ examples/instructions/src/lib.rs
@@ -7,28 +7,20 @@ struct InstructionsWorld;
 export!(InstructionsWorld);
 
 impl Guest for InstructionsWorld {
-    fn i32_from_s8(val: i8) {
+    fn s8_roundtrip(val: i8) -> i8 {
         assert!((i8::MIN..=i8::MAX).contains(&val));
+        val
     }
-    fn s8_from_i32() -> i8 {
-        Default::default()
-    }
-    fn i32_from_u8(val: u8) {
+    fn u8_roundtrip(val: u8) -> u8 {
         assert!((u8::MIN..=u8::MAX).contains(&val));
+        val
     }
-    fn u8_from_i32() -> u8 {
-        Default::default()
-    }
-    fn i32_from_s16(val: i16) {
+    fn s16_roundtrip(val: i16) -> i16 {
         assert!((i16::MIN..=i16::MAX).contains(&val));
+        val
     }
-    fn s16_from_i32() -> i16 {
-        Default::default()
-    }
-    fn i32_from_u16(val: u16) {
+    fn u16_roundtrip(val: u16) -> u16 {
         assert!((u16::MIN..=u16::MAX).contains(&val));
-    }
-    fn u16_from_i32() -> u16 {
-        Default::default()
+        val
     }
 }
