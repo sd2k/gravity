88a013fde98cb360c89757bce45c25a617be7e6a
Ben Sully <ben.sully88@gmail.com>
2025-09-26 16:47:36 +0100

    Add augurs-outlier example to repo to ease debugging

--- examples/generate.go
+++ examples/generate.go
@@ -4,8 +4,10 @@ package examples
 //go:generate cargo build -p example-records --target wasm32-unknown-unknown --release
 //go:generate cargo build -p example-iface-method-returns-string --target wasm32-unknown-unknown --release
 //go:generate cargo build -p example-instructions --target wasm32-unknown-unknown --release
+//go:generate sh -c "RUSTFLAGS='--cfg getrandom_backend=\"custom\"' cargo build -q -p example-outlier --target wasm32-unknown-unknown --release"
 
 //go:generate cargo run --bin gravity -- --world basic --output ./basic/basic.go ../target/wasm32-unknown-unknown/release/example_basic.wasm
 //go:generate cargo run --bin gravity -- --world records --output ./records/records.go ../target/wasm32-unknown-unknown/release/example_records.wasm
 //go:generate cargo run --bin gravity -- --world example --output ./iface-method-returns-string/example.go ../target/wasm32-unknown-unknown/release/example_iface_method_returns_string.wasm
 //go:generate cargo run --bin gravity -- --world instructions --output ./instructions/bindings.go ../target/wasm32-unknown-unknown/release/example_instructions.wasm
+//go:generate cargo run --bin gravity -- --world outlier --output ./outlier/outlier.go ../target/wasm32-unknown-unknown/release/example_outlier.wasm
