bb4b583ea739668172474ddc10e85d969bad25a4
Ben Sully <ben.sully88@gmail.com>
2025-09-26 16:56:15 +0100

    feat: include doc comments if WIT file is passed
    This PR emits doc comments from the WIT file into the generated Go code.
    It handles:
    
    - record docs
    - record field docs
    - variant docs
    - variant... variant docs
    - function docs
    - module docs
    
    Annoyingly, I had to add a separate flag to the CLI to allow passing the
    WIT file directly, because comments appear to be stripped from the
    component Wasm file.

--- examples/generate.go
+++ examples/generate.go
@@ -3,5 +3,5 @@ package examples
 //go:generate cargo build -p example-basic --target wasm32-unknown-unknown --release
 //go:generate cargo build -p example-iface-method-returns-string --target wasm32-unknown-unknown --release
 
-//go:generate cargo run --bin gravity -- --world basic --output ./basic/basic.go ../target/wasm32-unknown-unknown/release/example_basic.wasm
-//go:generate cargo run --bin gravity -- --world example --output ./iface-method-returns-string/example.go ../target/wasm32-unknown-unknown/release/example_iface_method_returns_string.wasm
+//go:generate cargo run --bin gravity -- --world basic --output ./basic/basic.go --wit-file ./basic/wit/basic.wit ../target/wasm32-unknown-unknown/release/example_basic.wasm
+//go:generate cargo run --bin gravity -- --world example --output ./iface-method-returns-string/example.go --wit-file ./iface-method-returns-string/wit/example.wit ../target/wasm32-unknown-unknown/release/example_iface_method_returns_string.wasm
